578e22780061dc30cdd754561e565b113b056f88
Oscar Wiberg <oscar.wiberg@sogeti.com>
2024-03-25 09:24:06 +0100

    added routing in app

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,7 +14,7 @@ export class RecipeService {
       new Ingredient('water', 3),
       new Ingredient('sugar', 2)
     ] ),
-    new Recipe('Burger', 'Super smash burger', 'https://www.marthastewart.com/thmb/8gzfd5woDpEDMFrzrRefp4Ye-S8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/04edf_bestburger_vert-b4226c48235e478f88d2d346ddf37d97.jpgitokeL-bvuyL', [
+    new Recipe('Burger', 'Super smash burger', 'https://img.hellofresh.com/c_fit,f_auto,fl_lossy,h_500,q_50,w_1900/hellofresh_s3/image/HF210603_R15_W29_SE_R12344114-1_KB_Main_low-bb2e5077.jpg', [
       new Ingredient('meat', 1),
       new Ingredient('bread', 2),
       new Ingredient('cheese', 1)
@@ -24,6 +24,10 @@ export class RecipeService {
 
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number){
+    return this.recipes[id];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
